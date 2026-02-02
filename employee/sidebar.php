c6204b022152f4173e687d4289bfdfeff27895ab
gyanendra26-web <gyanendragharti2063@gmail.com>
2026-02-02 15:38:50 +0545

    Delete employee directory

--- employee/sidebar.php
+++
@@ -1,8 +0,0 @@
-<nav class="sidebar">
-    <ul>
-        <li><a href="dashboard.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'dashboard.php' ? 'active' : ''; ?>">Dashboard</a></li>
-        <li><a href="attendance.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'attendance.php' ? 'active' : ''; ?>">Attendance History</a></li>
-        <li><a href="leave_request.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'leave_request.php' ? 'active' : ''; ?>">Leave Request</a></li>
-        <li><a href="profile.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'profile.php' ? 'active' : ''; ?>">My Profile</a></li>
-    </ul>
-</nav>
